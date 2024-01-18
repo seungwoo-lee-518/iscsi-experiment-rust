1d22152adca4750d5dfb9fc7ec5f3deb1caeeb13
SEUNGWOO LEE <seungwoo.lee.518@proton.me>
2024-01-18 04:27:06 +0000

    chore(flake.nix): add rust overlay

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Rust Dev Environments";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.rust-bin.stable.latest.default
+            ];
+          };
+        }
+      );
+}
